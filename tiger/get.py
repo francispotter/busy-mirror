405745633845768668f26d1e68d32eeb97434e3f
Francis Potter <francis@hathersage.group>
2018-02-16 15:03:38 -0800

    Handle plural and singular correctly in the get command

--- tiger/get.py
+++ tiger/get.py
@@ -12,19 +12,22 @@ from csv import DictReader
 from tiger import get_collection
 from tiger.selector import Selector
 
-def do(type, arguments):
+def do(itemtype, arguments):
+    is_plural = (itemtype == 'tasks')
     items = get_collection('tasks',['task'])
     if items:
-        selector = Selector(arguments, 1)
+        default = None if is_plural else 1
+        selector = Selector(arguments, default)
         indices = selector.indices([i['task'] for i in items])
         if indices:
+            if not is_plural: indices = indices[:1]
             results = [items[i]['task'] for i in indices]
             texts = [r[0].capitalize() + r[1:] for r in results]
             return '\n'.join(texts)
 
 def run():
     parser = argparse.ArgumentParser(prog="get")
-    parser.add_argument('type', default='task', choices=['task'], nargs='?')
+    parser.add_argument('type', default='task', choices=['task','tasks'], nargs='?')
     arguments, others = parser.parse_known_args()
     print(do(arguments.type, others))
 
