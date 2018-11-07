50b03fa55cde801c781a01505eeade98e245d66f
Francis Potter <git@fpotter.com>
2018-11-06 19:55:34 -0800

    Plan date as a tuple

--- test/test_task.py
+++ test/test_task.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+import datetime
 
 from tiger.task import Task
 
@@ -7,3 +8,17 @@ class TestTask(TestCase):
     def test_task(self):
         t = Task()
         self.assertIsInstance(t, Task)
+
+    def test_description_as_string(self):
+        t = Task('a')
+        self.assertEqual(t.description, 'a')
+
+    def test_plan(self):
+        t = Task()
+        d = datetime.date(2018,12,1)
+        t.convert_to_plan(d)
+
+    def test_plan_as_tuple(self):
+        t = Task()
+        t.convert_to_plan((2018,12,1))
+        self.assertEqual(t.plan_date.month, 12)
--- tiger/task.py
+++ tiger/task.py
@@ -1,10 +1,34 @@
+from datetime import date as Date
+
+TODO_STATE = 't'
+PLAN_STATE = 'p'
+DONE_STATE = 'd'
+
 class Task:
 
     def __init__(self, description=None):
         self._description = description or ''
+        self._state = TODO_STATE
+        self._plan_date = None
 
     def __str__(self):
         return self._description
+
+    @property
+    def description(self):
+        return self._description
+
+    def convert_to_plan(self, date):
+        self._state = PLAN_STATE
+        if isinstance(date, Date):
+            self._plan_date = date
+        elif isinstance(date, tuple):
+            self._plan_date = Date(*date)
+
+
+    @property
+    def plan_date(self):
+        return self._plan_date
 
 class Plan:
 
