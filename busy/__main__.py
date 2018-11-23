6c43297b7a68c5bacb73fcc568a1d08a9915b582
Francis Potter <git@fpotter.com>
2018-11-23 08:35:33 -0800

    Require Python 3.6.5 or higher

--- busy/__main__.py
+++ busy/__main__.py
@@ -3,6 +3,10 @@ import sys
 from .commander import Commander
 from .queue import Queue
 
-todo = Queue()
-commander = Commander(todo=todo)
-commander.handle_command(sys.argv)
+from busy import PYTHON_VERSION
+
+if sys.version_info < PYTHON_VERSION:
+    msg = "Busy requires Python version %i.%i.%i or higher" % PYTHON_VERSION
+    raise RuntimeError(msg)
+
+print("Command line processing will be implemented soon")
