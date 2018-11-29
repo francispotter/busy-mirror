82a8872c312283c634fb381ddde7876fa00c19ae
Francis Potter <git@fpotter.com>
2018-11-29 08:46:03 -0800

    Working on bringing back the pip module

--- setup.py
+++ setup.py
@@ -6,20 +6,18 @@ from setuptools import find_packages
 with open(os.path.join(os.path.dirname(__file__),'version')) as versionfile:
     version = versionfile.read().strip()
 
-commands_dir = os.path.join(os.path.dirname(__file__),'todo','commands')
-command_files = [f for f in os.listdir(commands_dir) if f[0] != '_']
-commands = [f.split('.')[0] for f in command_files]
-scripts = [f"{n}=todo.commands.{n}:run" for n in commands]
+# commands_dir = os.path.join(os.path.dirname(__file__),'todo','commands')
+# command_files = [f for f in os.listdir(commands_dir) if f[0] != '_']
+# commands = [f.split('.')[0] for f in command_files]
+# scripts = [f"{n}=todo.commands.{n}:run" for n in commands]
 
-setup(name='Todo',
+setup(name='Busy',
     version=version,
     description='Command-line task and plan management tool',
-    url='http://gitlab.com/francispotter/todo',
+    url='http://gitlab.com/francispotter/busy',
     author='Francis Potter',
-    author_email='todo@fpotter.com',
+    author_email='busy@fpotter.com',
     license='MIT',
     packages=find_packages(),
-    entry_points={'console_scripts':scripts},
-    # data_files=[('/usr/share/shellzoo/zshrc/',['todo.sh'])],
-    # package_data={'lemur':['lemur.ini']},
+    entry_points={'console_scripts':['busy']},
     zip_safe=False)
