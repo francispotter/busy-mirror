684b49b37b35688a5f3d545868fcb9cb29b986ad
Francis Potter <git@fpotter.com>
2018-11-29 09:01:50 -0800

    Only generate output if there's something to say

--- busy/__main__.py
+++ busy/__main__.py
@@ -7,6 +7,7 @@ def main():
     if sys.version_info < PYTHON_VERSION:
         msg = "Busy requires Python version %i.%i.%i or higher" % PYTHON_VERSION
         raise RuntimeError(msg)
-    print(Commander().handle(*sys.argv[1:]))
+    output = Commander().handle(*sys.argv[1:])
+    if output: print(output)
 
 if __name__ == '__main__': main()
