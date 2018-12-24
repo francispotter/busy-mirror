56fa31138174de29ea11fbb083281e07fd8b06ae
Francis Potter <git@fpotter.com>
2018-12-24 11:21:54 -0800

    Move activate into the PlanQueue class

--- busy/todo.py
+++ busy/todo.py
@@ -40,17 +40,35 @@ class Task(Item):
 class TodoQueue(Queue):
     itemclass = Task
 
+    def defer(self, date, *criteria):
+        indices = self.select(*(criteria or [1]))
+        plans = [self.get(i+1).as_plan(date) for i in indices]
+        self.plans.add(*plans)
+        self.delete_by_indices(*indices)
+
 
 class PlanQueue(TodoQueue):
     schema = ['plan_date', 'description']
     listfmt = "{1.plan_date:%Y-%m-%d}  {1.description}"
 
+    def activate(self, *criteria, today=False):
+        if today:
+            func = lambda t: t.plan_date <= busy.future.today()
+            indices = self.select(func)
+        else:
+            indices = self.select(*criteria)
+        tasks = [self.get(i+1).as_todo() for i in indices]
+        self.todos.add(*tasks, index=0)
+        self.delete_by_indices(*indices)
+
 
 class System:
 
     def __init__(self, *items, todos=None, plans=None):
-        self.todos = todos if todos else TodoQueue()
         self.plans = plans if plans else PlanQueue()
+        self.todos = todos if todos else TodoQueue()
+        self.todos.plans = self.plans
+        self.plans.todos = self.todos
         self.add(*items)
 
     def add(self, *items):
@@ -63,20 +81,10 @@ class System:
         self.todos.drop(*criteria)
 
     def defer(self, date, *criteria):
-        indices = self.todos.select(*(criteria or [1]))
-        plans = [self.todos.get(i+1).as_plan(date) for i in indices]
-        self.plans.add(*plans)
-        self.todos.delete_by_indices(*indices)
+        self.todos.defer(date, *criteria)
 
     def activate(self, *criteria, today=False):
-        if today:
-            func = lambda t: t.plan_date <= busy.future.today()
-            indices = self.plans.select(func)
-        else:
-            indices = self.plans.select(*criteria)
-        tasks = [self.plans.get(i+1).as_todo() for i in indices]
-        self.todos.add(*tasks, index=0)
-        self.plans.delete_by_indices(*indices)
+        self.plans.activate(*criteria, today=today)
 
     def manage(self, *criteria):
         tasklist = self.todos.list(*criteria)
